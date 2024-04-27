f36221af6d900a5ed4660026d97d244c09b02f2f
Gardeken <gardeken931@gmail.com>
2024-04-27 15:32:46 -0400

    conectando a la BD

--- views/adminPanel/js/api.js
+++ views/adminPanel/js/api.js
@@ -1,74 +1,69 @@
-const url = 'http://localhost:3000/menu'
+const url = "/api/foods";
 
 // consultar todo
-export async function consultarBD(){
-    try{
-    const consultar = await fetch(url)
-    const respuesta = await consultar.json()
-    return respuesta
-    }catch(err){
-        console.log(err);
-    }
-
-    
+export async function consultarBD() {
+  try {
+    const menu = await axios.get(url);
+    return menu.data;
+  } catch (err) {
+    return 404;
+  }
 }
 
 // agregar
 
-export async function agregarBD(producto){
-    try{
-        await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(producto),
-        })
-    }catch(err){
-        console.log(err)
-    }
+export async function agregarBD(producto) {
+  try {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(producto),
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 //eliminar
 
-export async function eliminardelaBD(id){
-    try{
-        await fetch(`${url}/${id}`, {
-            method: "DELETE"
-        })
-    }catch(err){
-        console.log(err)
-    }
+export async function eliminardelaBD(id) {
+  try {
+    await axios(url);
+    await fetch(`${url}/${id}`, {
+      method: "DELETE",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 //actualizar
 
-export async function editarBD(producto){
-    try{
-        await fetch(`${url}/${producto.id}`, {
-            method: "PUT",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(producto),
-        })
-    }catch(err){
-        console.log(err)
-    }
+export async function editarBD(producto) {
+  try {
+    await fetch(`${url}/${producto.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(producto),
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 //consultar por id
 
-export async function consultaIDBD(id){
-    try{
-    const consulta = await fetch(`${url}/${id}`)
-    const producto = await consulta.json()
+export async function consultaIDBD(id) {
+  try {
+    const consulta = await fetch(`${url}/${id}`);
+    const producto = await consulta.json();
 
-    return producto
-    
-    }catch(err){
-        return 404
-    }
-
-    
+    return producto;
+  } catch (err) {
+    return 404;
+  }
 }
--- views/adminPanel/js/index.js
+++ views/adminPanel/js/index.js
@@ -1,34 +1,37 @@
-import { consultarBD, agregarBD,editarBD,eliminardelaBD } from "./api.js"
+import { consultarBD, agregarBD, editarBD, eliminardelaBD } from "./api.js";
 
-const containerList = document.querySelector('#listado-Productos')
+const containerList = document.querySelector("#listado-Productos");
 const categorias = {
-    1: "Comidas",
-    2: "Postres",
-    3: "Bebidas"
-}
+  1: "Comidas",
+  2: "Postres",
+  3: "Bebidas",
+};
 
-document.addEventListener('DOMContentLoaded', mostrarProductos)
-containerList.addEventListener('click', deleteProduct)
+document.addEventListener("DOMContentLoaded", mostrarProductos);
+containerList.addEventListener("click", deleteProduct);
 
-async function deleteProduct(e){
-    if(e.target.classList.contains('eliminar')){
-        const id = parseInt(e.target.getAttribute('data-producto'))
-        console.log(id)
-        const confirmar = confirm('¿Quieres eliminar este producto?')
-        if(confirmar){
-            await eliminardelaBD(id)
-        }
+async function deleteProduct(e) {
+  if (e.target.classList.contains("eliminar")) {
+    const id = parseInt(e.target.getAttribute("data-producto"));
+    console.log(id);
+    const confirmar = confirm("¿Quieres eliminar este producto?");
+    if (confirmar) {
+      await eliminardelaBD(id);
     }
+  }
 }
 
-async function mostrarProductos(){
-    const listadoProductos = await consultarBD()
-    listadoProductos.forEach(product => {
-        const {id, nombre, precio, categoria} = product
+async function mostrarProductos() {
+  const listadoProductos = await consultarBD();
+  if (listadoProductos === 404) {
+    alert("Hubo un error al traer los productos");
+  }
+  listadoProductos.forEach((product) => {
+    const { id, nombre, precio, categoria } = product;
 
-        const row = document.createElement('tr')
-        
-        row.innerHTML = `
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
         <td class='px-5 py-4 border-b border-gray-200'>
             <p class='font-bold'>${nombre}</p>
         </td>
@@ -42,9 +45,8 @@ async function mostrarProductos(){
             <a href='editar-producto.html?id=${id}' class='text-teal-600 hover:text-teal-900 px-3'>Editar</a>
             <a href='#' data-producto='${id}' class='eliminar text-red-600 hover:text-red-900 px-3'>Eliminar</a>
         </td>
-        `
-        row.classList.add('py-3')
-        containerList.appendChild(row)
-
-    });
+        `;
+    row.classList.add("py-3");
+    containerList.appendChild(row);
+  });
 }
